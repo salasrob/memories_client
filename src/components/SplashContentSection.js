f25d1067faa9ea33182caa73e7e9a67bb6376ec9
salasrob <robertjsalas1@gmail.com>
2022-10-15 18:51:56 -0500

    created html bones for content section

---
+++ src/components/SplashContentSection.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SplashContentSection = () => {
+  return (
+    <article className='container'>
+        <div className='row'>
+            <div className='col'>
+                <image alt='test'/>
+            </div>
+            <div className='col'>
+                <h1>Content</h1>
+                <p>
+                Your event website is ready; now, it's time to build your guestlist. Invite attendees to your event with beautifully-simple and fully customizable email invitations. In addition to powerful design customization, EventCreate's invite system comes pre-built with a host of features including delivery tracking, bulk list upload and automatic reminders. Don't want to send invites for your event? No problem. You can promote your event website in other ways, including partner site promotion, search engine optimization, social sharing, referrals or by embedding buttons on another website.
+                </p>
+            </div>
+        </div>
+    </article>
+  )
+}
+
+export default SplashContentSection
